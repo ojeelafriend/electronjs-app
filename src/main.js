20ed0008655dfc4fa9a320b78529fb9898b5d1e4
odev-badwin <ts8irolon@gmail.com>
2021-11-29 21:09:30 -0300

    create basic handle windows no crud

--- src/main.js
+++ src/main.js
@@ -1,3 +1,35 @@
-const { app, BrowserView } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 
-app.on('ready', () => {});
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow.loadFile('../index.html');
+
+  const mainMenu = Menu.buildFromTemplate(menuLayout);
+  Menu.setApplicationMenu(mainMenu);
+});
+
+app.on('will-quit', () => {
+  console.log('Bye bye');
+});
+
+const createNewCard = () => {
+  let newCard = new BrowserWindow({ width: 400, height: 200 });
+  newCard.loadFile('../newCard.html');
+};
+
+const menuLayout = [
+  {
+    label: 'Card',
+    submenu: [
+      {
+        label: 'Create card',
+        accelerator: 'Ctrl+N',
+        click() {
+          createNewCard();
+        },
+      },
+    ],
+  },
+];
